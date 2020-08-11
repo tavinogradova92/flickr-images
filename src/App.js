31484c0b80ec8f05a1798ab14f9807557c804393
Tanja <tavinogradova92@gmail.com>
2020-08-11 22:33:36 +0200

    Search box working with a hardcoded data

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { photos } from './photos';
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>Flickr Gallery Imitator</h1>
-            <SearchBox />
-            <CardList photos={photos}/>
-        </div>
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            photos: photos,
+            searchfield: ''
+        }
+    }
+    
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
         
-    )
+    }
+
+    render() {
+        const filteredPhotos = this.state.photos.filter(photos => {
+            return photos.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        return (
+            <div className='tc'>
+                <h1>Flickr Gallery Imitator</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList photos={filteredPhotos}/>
+            </div>
+        );
+    }
+    
 }
 
 export default App;
