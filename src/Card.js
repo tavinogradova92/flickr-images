5ef36000692635beacdcae28e01bb8276f815431
Tanja <tavinogradova92@gmail.com>
2020-08-11 15:22:01 +0200

    Preparing Card component

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Card.css';
 
 const Card = () => {
     return (
         <div>
-            <img alt='photo' src='' />
+            <img alt='gallery' className="w-10" src='https://img.etimg.com/thumb/msid-68721417,width-1070,height-580,imgsize-1016106,overlay-etpanache/photo.jpg' />
             <div>
                 <h2>Jane Doe</h2>
                 <p>jane.doe@gmail.com</p>
