b9fa75a90f975a9d718ecec6a196bdd2cff85586
Tanja <tavinogradova92@gmail.com>
2020-08-13 16:10:19 +0200

    Gallery designed. Flexbox and grid used

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,15 +3,16 @@ import './Card.css';
 
 const Card = ({title, id, url }) => {
     return (
-        <div 
-            className='tc dib br3 pa2 ma2 grow bw2 shadow-5'>
-                <img   
-                    alt={title}  
-                    src={url} />
-                <div>
-                    <h2>{title}</h2>
-                </div>
+        <div className='column dib ma2 grow  shadow-5'>
+        
+            <img   
+                alt={title}  
+                src={url}
+            />
+    
+       
         </div>
+        
     );
 }
 
