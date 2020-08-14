26697648dda0f061158daf92b54a56edf6fb2963
Tanja <tavinogradova92@gmail.com>
2020-08-14 11:54:35 +0200

    Updates

---
+++ precache-manifest.df540ed4747bb1c903357f25cb6bf2a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48713559c2cec1895d1b353d9330b963",
+    "url": "/flickr-images/index.html"
+  },
+  {
+    "revision": "d5d4fd5c2b8272d1bf07",
+    "url": "/flickr-images/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ddcb0617dde423b6fc89",
+    "url": "/flickr-images/static/css/main.2183f7b7.chunk.css"
+  },
+  {
+    "revision": "d5d4fd5c2b8272d1bf07",
+    "url": "/flickr-images/static/js/2.7df94fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flickr-images/static/js/2.7df94fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddcb0617dde423b6fc89",
+    "url": "/flickr-images/static/js/main.7d1eba17.chunk.js"
+  },
+  {
+    "revision": "d9af0956c71d713fdf1e",
+    "url": "/flickr-images/static/js/runtime-main.8bc467aa.js"
+  },
+  {
+    "revision": "238679babe571fde5821257556e6ab85",
+    "url": "/flickr-images/static/media/Republiko-x3nJ0.238679ba.woff"
+  }
+]);
