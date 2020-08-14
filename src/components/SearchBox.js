b6289f3c6a85edbafd029c38abe07d0908468923
Tanja <tavinogradova92@gmail.com>
2020-08-14 10:34:23 +0200

    Footer and responsiveness improved

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const SearchBox = ({ searchfield, searchChange }) => {
     return (
-        <div className='pa2'>
+        <div className='pa1'>
             <input 
-                className='pa3 ba b--grey'
+                className='pa2 ba b--grey'
                 type='search' 
                 placeholder='search photos' 
                 onChange={searchChange}
